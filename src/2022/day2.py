c7b45551e570d07dacb8f44d1a97f8f21987d921
tthompson2 <tthompson2@eprod.com>
2022-12-02 11:48:25 -0600

    squash part 1

--- src/2022/day2.py
+++ src/2022/day2.py
@@ -1,31 +1,22 @@
 from utils.utils import pull_input_directly
 
 
-def part_1(rounds, winkeys, rps_scores):
-    drawkeys = {"A": "X", "B": "Y", "C": "Z"}
-    score = 0
+def part_1(rounds):
+    newkeys = {
+        "A": {"X": 4, "Y": 8, "Z": 3},
+        "B": {"X": 1, "Y": 5, "Z": 9},
+        "C": {"X": 7, "Y": 2, "Z": 6}
+        }
     
-    for r in rounds:
-        opponent = r[0]
-        me = r[-1]
-        
-        if drawkeys[opponent] == me:
-            # draw
-            score += 3 + rps_scores[me]
-        elif winkeys[opponent] == me:
-            # win
-            score += 6 + rps_scores[me]
-        else:
-            # loss
-            score += rps_scores[me]
-        
-    return score
+    return sum(newkeys[r[0]][r[-1]] for r in rounds)
 
-def part_2(rounds, winkeys, rps_scores):
+
+def part_2(rounds):
+    rps_scores = {"X": 1, "Y": 2, "Z": 3}
     all_outcomes = {
         "X": {"A": "Z", "B": "X", "C": "Y", "val": 0},
         "Y": {"A": "X", "B": "Y", "C": "Z", "val": 3},
-        "Z": winkeys
+        "Z": {"A": "Y", "B": "Z", "C": "X", "val": 6}
     }
     
     return sum(rps_scores[all_outcomes[r[-1]][r[0]]] + all_outcomes[r[-1]]["val"] for r in rounds)
@@ -34,10 +25,11 @@ def part_2(rounds, winkeys, rps_scores):
 if __name__ == "__main__":
     rounds = pull_input_directly(2022, 2)[:-1]
     winkeys = {"A": "Y", "B": "Z", "C": "X", "val": 6}
-    rps_scores = {"X": 1, "Y": 2, "Z": 3}
+    
 
-    part1_score = part_1(rounds, winkeys, rps_scores)
+    part1_score = part_1(rounds)
     
-    part2_score = part_2(rounds, winkeys, rps_scores)
+    part2_score = part_2(rounds)
     
-    print(part2_score)
+    print(f"Part 1: {part1_score}")
+    print(f"Part 2: {part2_score}")
