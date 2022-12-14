1bf3fc1f706e712b6e328072e847c4bb2a557bdd
tthompson2 <tthompson2@eprod.com>
2022-12-14 08:51:44 -0600

    very ugly day 9 solution complete

--- src/2022/day9.py
+++ src/2022/day9.py
@@ -5,64 +5,63 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    # moves = pull_input_directly(2022, 9)[:-1]
-    moves = read_input("C:\\Users\\tthompson2\\Documents\\REPOS\\AdventOfCode\\src\\2022\\day9tstinp.txt")
+    moves = pull_input_directly(2022, 9)[:-1]
 
-    # grid = np.zeros([500, 500], dtype=np.int8)
-    # dfgrid = pd.DataFrame(
-    #     {
-    #         i: [None] * 500
-    #         for i in range(500)
-    #     }
-    # )
-    # headx = heady = 250
-    # tailx = taily = 250
+    grid = np.zeros([500, 500], dtype=np.int8)
+    dfgrid = pd.DataFrame(
+        {
+            i: [None] * 500
+            for i in range(500)
+        }
+    )
+    headx = heady = 250
+    tailx = taily = 250
 
-    # grid[headx, heady] = 2
+    grid[headx, heady] = 2
 
-    # ### PART 1 ###
-    # for i, move in enumerate(moves):
-    #     dfgrid.iloc[headx, heady] = "H"
-    #     direction = move.split(" ")[0]
-    #     steps = int(move.split(" ")[1])
+    ### PART 1 ###
+    for i, move in enumerate(moves):
+        dfgrid.iloc[headx, heady] = "H"
+        direction = move.split(" ")[0]
+        steps = int(move.split(" ")[1])
 
-    #     for step in range(steps):
-    #         dfgrid.iloc[headx, heady] = None
-    #         dfgrid.iloc[tailx, taily] = None
-    #         # move head
-    #         if direction == "L":
-    #             heady -= 1
-    #         elif direction == "R":
-    #             heady += 1
-    #         elif direction == "U":
-    #             headx -= 1
-    #         elif direction == "D":
-    #             headx += 1
+        for step in range(steps):
+            dfgrid.iloc[headx, heady] = None
+            dfgrid.iloc[tailx, taily] = None
+            # move head
+            if direction == "L":
+                heady -= 1
+            elif direction == "R":
+                heady += 1
+            elif direction == "U":
+                headx -= 1
+            elif direction == "D":
+                headx += 1
 
-    #         # does tail need to move?
-    #         if abs(headx - tailx) > 1 or abs(heady - taily) > 1:
-    #             if direction == "L":
-    #                 tailx = headx
-    #                 taily = heady + 1
-    #             elif direction == "R":
-    #                 tailx = headx
-    #                 taily = heady - 1
-    #             elif direction == "U":
-    #                 tailx = headx + 1
-    #                 taily = heady
-    #             elif direction == "D":
-    #                 tailx = headx - 1
-    #                 taily = heady
-
-
-    #         # grid[headx, heady] = 0
-
-    #         grid[tailx, taily] = 1
-    #         dfgrid.iloc[tailx, taily] = "T"
-    #         dfgrid.iloc[headx, heady] = "H"
+            # does tail need to move?
+            if abs(headx - tailx) > 1 or abs(heady - taily) > 1:
+                if direction == "L":
+                    tailx = headx
+                    taily = heady + 1
+                elif direction == "R":
+                    tailx = headx
+                    taily = heady - 1
+                elif direction == "U":
+                    tailx = headx + 1
+                    taily = heady
+                elif direction == "D":
+                    tailx = headx - 1
+                    taily = heady
 
 
-    # print(np.sum(grid))
+            # grid[headx, heady] = 0
+
+            grid[tailx, taily] = 1
+            dfgrid.iloc[tailx, taily] = "T"
+            dfgrid.iloc[headx, heady] = "H"
+
+
+    print(np.sum(grid))
     
     ### PART 2 ###
     grid = np.zeros([500, 500], dtype=np.int8)
@@ -72,17 +71,16 @@ if __name__ == "__main__":
             for i in range(500)
         }
     )
-    knots = [[250, 250] for i in range(10)]
+    knots = [[250, 250] for _ in range(10)]
 
     for i, move in enumerate(moves):
         direction = move.split(" ")[0]
         steps = int(move.split(" ")[1])
 
         for step in range(steps):
-            # for k in knots:
-            #     dfgrid.iloc[k[0], k[1]] = None
-            # dfgrid.iloc[knots, heady] = None
-            # dfgrid.iloc[tailx, taily] = None
+            for k in knots:
+                dfgrid.iloc[k[0], k[1]] = None
+
             # move head
             if direction == "L":
                 knots[0][1] -= 1
@@ -110,13 +108,10 @@ if __name__ == "__main__":
                         knots[j][0] += 1
 
 
-            # grid[headx, heady] = 0
-            # for z, k in enumerate(knots):
-            #     dfgrid.iloc[k[0], k[1]] = z
+            for z, k in enumerate(knots):
+                dfgrid.iloc[k[0], k[1]] = z
 
             grid[knots[-1][0], knots[-1][1]] = 1
-            # dfgrid.iloc[tailx, taily] = "T"
-            # dfgrid.iloc[headx, heady] = "H"
 
 
     print(np.sum(grid))
