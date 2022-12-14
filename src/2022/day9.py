d2efefaf919cfda7c01826166c09d68db8c0de63
tthompson2 <tthompson2@eprod.com>
2022-12-14 08:05:52 -0600

    day9 pt2 progress

--- src/2022/day9.py
+++ src/2022/day9.py
@@ -1,12 +1,70 @@
-from utils.utils import read_input
+from utils.utils import pull_input_directly, read_input
 import numpy as np
 import pandas as pd
 
 
 
 if __name__ == "__main__":
-    moves = read_input("day9inp.txt")
+    # moves = pull_input_directly(2022, 9)[:-1]
+    moves = read_input("C:\\Users\\tthompson2\\Documents\\REPOS\\AdventOfCode\\src\\2022\\day9tstinp.txt")
 
+    # grid = np.zeros([500, 500], dtype=np.int8)
+    # dfgrid = pd.DataFrame(
+    #     {
+    #         i: [None] * 500
+    #         for i in range(500)
+    #     }
+    # )
+    # headx = heady = 250
+    # tailx = taily = 250
+
+    # grid[headx, heady] = 2
+
+    # ### PART 1 ###
+    # for i, move in enumerate(moves):
+    #     dfgrid.iloc[headx, heady] = "H"
+    #     direction = move.split(" ")[0]
+    #     steps = int(move.split(" ")[1])
+
+    #     for step in range(steps):
+    #         dfgrid.iloc[headx, heady] = None
+    #         dfgrid.iloc[tailx, taily] = None
+    #         # move head
+    #         if direction == "L":
+    #             heady -= 1
+    #         elif direction == "R":
+    #             heady += 1
+    #         elif direction == "U":
+    #             headx -= 1
+    #         elif direction == "D":
+    #             headx += 1
+
+    #         # does tail need to move?
+    #         if abs(headx - tailx) > 1 or abs(heady - taily) > 1:
+    #             if direction == "L":
+    #                 tailx = headx
+    #                 taily = heady + 1
+    #             elif direction == "R":
+    #                 tailx = headx
+    #                 taily = heady - 1
+    #             elif direction == "U":
+    #                 tailx = headx + 1
+    #                 taily = heady
+    #             elif direction == "D":
+    #                 tailx = headx - 1
+    #                 taily = heady
+
+
+    #         # grid[headx, heady] = 0
+
+    #         grid[tailx, taily] = 1
+    #         dfgrid.iloc[tailx, taily] = "T"
+    #         dfgrid.iloc[headx, heady] = "H"
+
+
+    # print(np.sum(grid))
+    
+    ### PART 2 ###
     grid = np.zeros([500, 500], dtype=np.int8)
     dfgrid = pd.DataFrame(
         {
@@ -14,56 +72,53 @@ if __name__ == "__main__":
             for i in range(500)
         }
     )
-    headx = heady = 250
-    tailx = taily = 250
-
-    grid[headx, heady] = 2
+    knots = [[250, 250] for i in range(10)]
 
     for i, move in enumerate(moves):
-        grid[headx, heady] = 0 if grid[headx, heady] != 1 else 1
-        dfgrid.iloc[headx, heady]
-        dfgrid.iloc[headx, heady] = "H"
-
-        prev_dir = moves[i-1].split(" ")[0] if i > 0 else None
         direction = move.split(" ")[0]
         steps = int(move.split(" ")[1])
 
         for step in range(steps):
-            dfgrid.iloc[headx, heady] = None
-            dfgrid.iloc[tailx, taily] = None
+            # for k in knots:
+            #     dfgrid.iloc[k[0], k[1]] = None
+            # dfgrid.iloc[knots, heady] = None
+            # dfgrid.iloc[tailx, taily] = None
             # move head
             if direction == "L":
-                heady -= 1
+                knots[0][1] -= 1
             elif direction == "R":
-                heady += 1
+                knots[0][1] += 1
             elif direction == "U":
-                headx -= 1
+                knots[0][0] -= 1
             elif direction == "D":
-                headx += 1
+                knots[0][0] += 1
 
             # does tail need to move?
-            if abs(headx - tailx) > 1 or abs(heady - taily) > 1:
-                if direction == "L":
-                    tailx = headx
-                    taily = heady + 1
-                elif direction == "R":
-                    tailx = headx
-                    taily = heady - 1
-                elif direction == "U":
-                    tailx = headx + 1
-                    taily = heady
-                elif direction == "D":
-                    tailx = headx - 1
-                    taily = heady
+            for j in range(1, 10):
+                if abs(knots[j-1][0] - knots[j][0]) > 1 or abs(knots[j-1][1] - knots[j][1]) > 1:
+                    diffx = knots[j-1][1] - knots[j][1]
+                    diffy = knots[j-1][0] - knots[j][0]
+                    
+                    if diffx < 0:
+                        knots[j][1] -= 1
+                    elif diffx > 0:
+                        knots[j][1] += 1
+                        
+                    if diffy < 0:
+                        knots[j][0] -= 1
+                    elif diffy > 0:
+                        knots[j][0] += 1
 
 
-            grid[headx, heady] = 0
+            # grid[headx, heady] = 0
+            # for z, k in enumerate(knots):
+            #     dfgrid.iloc[k[0], k[1]] = z
 
-            grid[tailx, taily] = 1
-            dfgrid.iloc[tailx, taily] = "T"
-            dfgrid.iloc[headx, heady] = "H"
+            grid[knots[-1][0], knots[-1][1]] = 1
+            # dfgrid.iloc[tailx, taily] = "T"
+            # dfgrid.iloc[headx, heady] = "H"
 
 
-    print(np.where(grid==1))
+    print(np.sum(grid))
 
 
