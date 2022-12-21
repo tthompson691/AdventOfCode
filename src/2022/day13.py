c925be864242a12ad22a1bdf0d61fa8385cf48bd
tthompson2 <tthompson2@eprod.com>
2022-12-21 11:10:59 -0600

    day 13 done

--- src/2022/day13.py
+++ src/2022/day13.py
@@ -1,33 +1,41 @@
 from utils.utils import pull_input_directly, read_input
-
+import re
 
 def compare(pp_l, pp_r):
     if type(pp_l) == list and type(pp_r) == int:
         pp_r = [pp_r]
+        return compare(pp_l, pp_r)
     elif type(pp_l) == int and type(pp_r) == list:
         pp_l = [pp_l]
+        return compare(pp_l, pp_r)
     elif type(pp_l) == int and type(pp_r) == int:
         if pp_l > pp_r:
             return False
         elif pp_l < pp_r:
             return True
+        else:
+            return None
     else:
-        if len(pp_l) == 0 and len(pp_r) != 0:
-            return True
-        for j in range(len(pp_l)):
+        if len(pp_r) == 0 and len(pp_l) != 0:
+            return False
+        for j in range(len(pp_r)):
             try:
-                return compare(pp_l[j], pp_r[j])
+                ans = compare(pp_l[j], pp_r[j])
+                if ans is not None:
+                    return ans
+                else:
+                    continue
             except IndexError:
-                return False
+                return True
 
 
 if __name__ == "__main__":
-    # packets = pull_input_directly(2022, 13, delimiter="\\n\\n")
-    packets = read_input("day13tst.txt", "\n\n")
     packet_pairs = [
-        (eval(p.split("\n")[0]), eval(p.split("\n")[1]))
-        for p in packets
+        (eval(p.split("\\n")[0]), eval(p.split("\\n")[1]))
+        for p in pull_input_directly(2022, 13, delimiter="\\n\\n")
     ]
+
+    p2_packets = [eval(p) for p in pull_input_directly(2022, 13) if p != ""] + [[[2]], [[6]]]
 
     p1_ans = 0
     for i, packpair in enumerate(packet_pairs):
@@ -35,3 +43,17 @@ if __name__ == "__main__":
             p1_ans += i + 1
 
     print(p1_ans)
+
+    ordered_packets = [p2_packets[0]]
+    for p in p2_packets[1:]:
+        x = 0
+        for x, op in enumerate(ordered_packets):
+            if compare(p, op):
+                ordered_packets = ordered_packets[:x] + [p] + ordered_packets[x:]
+                break
+            elif x == len(ordered_packets) - 1:
+                ordered_packets = ordered_packets + [p]
+            else:
+                x += 1
+
+    print((ordered_packets.index([[2]]) + 1) * (ordered_packets.index([[6]]) + 1))
