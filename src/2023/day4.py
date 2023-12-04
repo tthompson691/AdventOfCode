63a6aa845da390e6857793a35ba845a8fed8ef55
tthompson691 <tthompson691@gmail.com>
2023-12-04 10:13:03 -0600

    2023.4 done

---
+++ src/2023/day4.py
@@ -0,0 +1,37 @@
+import re
+
+from utils.utils import pull_input_directly
+
+
+def determine_card_value(num_winners):
+    if num_winners != 0:
+        return 2 ** (num_winners - 1)
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    cards = pull_input_directly(2023, 4)[:-1]
+
+    total_value = 0
+    card_dict = {}
+    for card in cards:
+        winners, ours = card.split(" | ")
+        card_num = int(re.findall("[0-9]+", winners.split(": ")[0])[0])
+        card_dict[card_num] = {}
+        winners = set(map(int, re.findall("[0-9]+", winners.split(": ")[1])))
+        ours = set(map(int, re.findall("[0-9]+", ours)))
+        num_winners = len(winners.intersection(ours))
+        total_value += determine_card_value(num_winners)
+
+        card_dict[card_num]["total_owned"] = 1
+        card_dict[card_num]["num_winners"] = num_winners
+
+    print(f"PART 1: {total_value}")
+
+    for card in card_dict:
+        for x in range(card + 1, card + 1 + card_dict[card]["num_winners"]):
+            card_dict[x]["total_owned"] += card_dict[card]["total_owned"]
+
+    p2_sum = sum([v["total_owned"] for _, v in card_dict.items()])
+    print(f"PART 2: {p2_sum}")
