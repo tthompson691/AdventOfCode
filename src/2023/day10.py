138384c95aa2e61d35e9c27bef510d6c1d99b365
tthompson691 <56414380+tthompson691@users.noreply.github.com>
2023-12-11 16:44:15 -0600

    2023.11 done

---
+++ src/2023/day10.py
@@ -0,0 +1,36 @@
+from utils.utils import pull_input_directly
+import numpy as np
+
+def next_step(val, dir):
+    northturn = (-1, 0, "n")
+    southturn = (1, 0, "s")
+    eastturn = (0, 1, "e")
+    westturn = (0, -1, "w")
+    return {
+        "|": {"s": southturn, "n": northturn},
+        "-": {"e": eastturn, "w": westturn},
+        "L": {"s": eastturn, "w": northturn},
+        "F": {"n": eastturn, "w": southturn},
+        "J": {"s": westturn, "e": northturn},
+        "7": {"n": westturn, "e": southturn}
+    }[val][dir]
+
+if __name__ == "__main__":
+    # with open("/Users/travisthompson/REPOS/AdventOfCode/src/2023/day10samp.txt", "r") as f:
+    #     inp = np.array([[*x] for x in f.read().splitlines()])
+    inp = np.array([[*x] for x in pull_input_directly(2023, 10)[:-1]])
+
+    x, y = [i[0] for i in np.where(inp == "S")]
+
+    dir = "s"
+    steps_taken = 1
+    x += 1
+    while inp[x][y] != "S":
+        dx, dy, dir = next_step(inp[x][y], dir)
+        steps_taken += 1
+        x += dx
+        y += dy
+
+    print(f"PART 1: {int(steps_taken / 2)}")
+
+
--- src/2023/day5.py
+++ src/2023/day5.py
@@ -23,63 +23,44 @@ if __name__ == "__main__":
         elif line == '' and map_vals != []:
             almanac[map_title] = pd.DataFrame(map_vals, columns=["destination", "source", "range"])
             map_vals = []
+    #
+    # min_loc = None
+    # for seed in seeds:
+    #     seed_path = [seed]
+    #     for _, mapper in almanac.items():
+    #         cur_step = seed_path[-1]
+    #         matching_range = mapper[
+    #             (cur_step >= mapper["source"]) & (cur_step < mapper["source"] + mapper["range"])
+    #         ].reset_index(drop=True)
+    #         if not matching_range.empty:
+    #             diff = cur_step - matching_range.loc[0, "source"]
+    #             seed_path.append(matching_range.loc[0, "destination"] + diff)
+    #
+    #     if not min_loc:
+    #         min_loc = seed_path[-1]
+    #     elif seed_path[-1] < min_loc:
+    #         min_loc = seed_path[-1]
 
-    min_loc = None
-    for seed in seeds:
-        seed_path = [seed]
+    # print(f"PART 1: {min_loc}")
+
+    p2_seeds = [(seeds[x], seeds[x] + seeds[x+1] - 1) for x in range(0, len(seeds)-1, 2)]
+
+    for seedmin, seedmax in p2_seeds:
+        source_ranges = {"mapped": {"min": None, "max": None}, "unmapped": {"min": None, "max": None}}
         for _, mapper in almanac.items():
-            cur_step = seed_path[-1]
-            matching_range = mapper[
-                (cur_step >= mapper["source"]) & (cur_step < mapper["source"] + mapper["range"])
-            ].reset_index(drop=True)
-            if not matching_range.empty:
-                diff = cur_step - matching_range.loc[0, "source"]
-                seed_path.append(matching_range.loc[0, "destination"] + diff)
+            matching_ranges = mapper[
+                ((mapper["source"] <= seedmin) & (seedmin <= mapper["source"] + mapper["range"]))
+                | ((mapper["source"] <= seedmax) & (seedmax <= mapper["source"] + mapper["range"]))
+            ]
+            if not matching_ranges.empty:
+                diffs = matching_range["source"] - matching_range["destination"]
 
-        if not min_loc:
-            min_loc = seed_path[-1]
-        elif seed_path[-1] < min_loc:
-            min_loc = seed_path[-1]
 
-    print(f"PART 1: {min_loc}")
 
-    p2_seeds = [(seeds[x], seeds[x+1]) for x in range(0, len(seeds)-1, 2)]
 
     p2_ans = None
-    for seedrange in p2_seeds:
-        sourcemin = seedrange[0]
-        sourcemax = sum(seedrange) - 1
-        for a, mapper in almanac.items():
-            print(f"CHECKING {a}")
-            matching_ranges = mapper[(
-                    ((mapper["source"] <= sourcemin) & (mapper["source"] + mapper["range"]) >= sourcemin)
-             | ((mapper["source"] <= sourcemax) & (mapper["source"] + mapper["range"] >= sourcemax))
-            )].reset_index(drop=True)
-            if not matching_ranges.empty:
-                diff = matching_ranges.loc[0, "destination"] - matching_ranges.loc[0, "source"]
-                if (
-                        matching_ranges.loc[0, "source"]
-                        <= sourcemin <
-                        matching_ranges.loc[0, "source"] + matching_ranges.loc[0, "range"]
-                ):
-                    sourcemin = sourcemin + diff
-
-                if (
-                        matching_ranges.loc[0, "source"]
-                        <= sourcemax <
-                        matching_ranges.loc[0, "source"] + matching_ranges.loc[0, "range"]
-                ):
-                    sourcemax = sourcemax + diff
-                sourcemin
-
-        if not p2_ans:
-            p2_ans = sourcemin
-        elif sourcemin < p2_ans:
-            p2_ans = sourcemin
 
 
-            matching_ranges
-    sourcemin
 
-
+    p2_ans
 
