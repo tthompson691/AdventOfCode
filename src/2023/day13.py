ddd72c5ea70d4f9893e7b9b502caeca27681e034
tthompson691 <tthompson691@gmail.com>
2023-12-13 16:45:34 -0600

    2023.13 progress

--- src/2023/day13.py
+++ src/2023/day13.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-from utils.utils import pull_input_directly
-
 
 def check_for_mirrors(pattern):
     mirror_found = False
@@ -32,26 +30,36 @@ def check_for_mirrors(pattern):
 
 
 if __name__ == "__main__":
-    inp = [x.split("\\n") for x in pull_input_directly(2023, 13, delimiter="\\n\\n")]
-    inp[-1] = inp[-1][:-1]
+    with open(r"C:\Users\tthompson2\Documents\REPOS\AdventOfCode\src\2023\day13samp.txt") as f:
+        inp = [x.splitlines() for x in f.read().split("\n\n")]
+
+    # inp = [x.split("\\n") for x in pull_input_directly(2023, 13, delimiter="\\n\\n")]
+    # inp[-1] = inp[-1][:-1]
     patterns = [np.array([[*x] for x in y]) for y in inp]
 
     ### PART 1 ###
-    p1 = sum([check_for_mirrors(p) for p in patterns])
-    print(f"PART 1: {p1}")
+    p1_res = [check_for_mirrors(p) for p in patterns]
+    print(f"PART 1: {sum(p1_res)}")
 
     ### PART 2 (incomplete)###
     p2 = 0
-    for p in patterns:
+    for n, p in enumerate(patterns):
         mirror_found = False
         for i, x in enumerate(p):
             for j, y in enumerate(x):
-                p[i, j] = {"#": ".", ".": "#"}[p[i, j]]
+                p[i, j] = {"#": ".", ".": "#"}[y]
                 val = check_for_mirrors(p)
-                if val != 0:
+                if val != 0 and val != p1_res[n]:
                     p2 += val
                     mirror_found = True
+                    break
                 else:
                     p[i, j] = {"#": ".", ".": "#"}[p[i, j]]
+
+            if mirror_found:
+                break
+
+        # if not mirror_found:
+        #     p2 += p1_res[n]
 
     print(f"PART 2: {p2}")
