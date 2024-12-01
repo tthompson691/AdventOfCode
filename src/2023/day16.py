b2545e2cdd414707adca6157974d4ab5cb77db92
tthompson691 <56414380+tthompson691@users.noreply.github.com>
2024-12-01 10:47:20 -0600

    day16 p1 done, p2 in progress

--- src/2023/day16.py
+++ src/2023/day16.py
@@ -90,12 +90,13 @@ if __name__ == "__main__":
             + [[[0, y, "d"]]for y in range(max_y+1)]
             + [[[max_x, y, "u"]] for y in range(max_y+1)]
     )
-    # res = Parallel(n_jobs=-1)(delayed(do_the_thing)(x) for x in tqdm(starting_beams))
-    res = []
-    for s in tqdm(starting_beams):
-        res.append(do_the_thing(s))
-
+    res = Parallel(n_jobs=-1)(delayed(do_the_thing)(x) for x in tqdm(starting_beams))
+    # res = []
+    # for s in tqdm(starting_beams):
+    #     res.append(do_the_thing(s))
     print(max(res))
+    with open("/Users/travisthompson/REPOS/AdventOfCode/src/2023/day16res.txt", "r") as f:
+        f.write(f"{max(res)}")
 
 
 
