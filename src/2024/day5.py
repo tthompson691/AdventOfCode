175f7967d056816e804cc1f805dbb954d6922b51
tthompson691 <tthompson691@gmail.com>
2024-12-05 10:42:27 -0600

    2024 day 5 done

--- src/2024/day5.py
+++ src/2024/day5.py
@@ -13,22 +13,25 @@ def tree_search_order(ordered_pages_searched):
                 return res
 
 
-inp = read_input(2024, 5, source="sample")
+inp = read_input(2024, 5, source="real")
 rules = [tuple(map(int, r.split("|"))) for r in inp[: inp.index("")]]
 updates = [list(map(int, u.split(","))) for u in inp[inp.index("") + 1 :]]
-
-first_pages = [rule[0] for rule in rules]
-second_pages = [rule[1] for rule in rules]
-
-first_page = list(set(first_pages).difference(set(second_pages)))[0]
-LAST_PAGE = list(set(second_pages).difference(set(first_pages)))[0]
-ALL_PAGES = list(set(first_pages).union(set(second_pages)))
-RULE_DICT = {fp: [r[1] for r in rules if r[0] == fp] for fp in first_pages}
-
-
-order = tree_search_order([first_page])
-
+p1_sum = p2_sum = 0
 for update in updates:
-    order_indexes = [order.index(v) for v in update]
-    if all([order_indexes[i] > order_indexes[i - 1] for i in range(1, len(order_indexes))]):
-        print(f"{update} is valid!")
+    applicable_rules = [rule for rule in rules if all([r in update for r in rule])]
+    first_pages = [rule[0] for rule in applicable_rules]
+    second_pages = [rule[1] for rule in applicable_rules]
+
+    first_page = list(set(first_pages).difference(set(second_pages)))[0]
+    LAST_PAGE = list(set(second_pages).difference(set(first_pages)))[0]
+    ALL_PAGES = list(set(first_pages).union(set(second_pages)))
+    RULE_DICT = {fp: [r[1] for r in applicable_rules if r[0] == fp] for fp in first_pages}
+
+    order = tree_search_order([first_page])
+    if order == update:
+        p1_sum += update[len(update) // 2]
+    else:
+        p2_sum += order[len(order) // 2]
+
+print(f"PART 1: {p1_sum}")
+print(f"PART 2: {p2_sum}")
