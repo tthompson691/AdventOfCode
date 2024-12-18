86652d4b7dbcccdcf386039743a920ead86fd8e4
tthompson691 <tthompson691@gmail.com>
2024-12-18 10:47:50 -0600

    2024 day 16 done

--- src/2024/day16.py
+++ src/2024/day16.py
@@ -4,57 +4,49 @@ import numpy as np
 
 from utils.utils import read_input
 
-# def highlight_path(maze, path):
-#     for i, j in path:
-#         maze[i, j] = "X"
 
-#     print(maze)
-
-# def step_once(i, j, score, visited, heading=None):
-#     global FINAL_SCORES
-#     if maze[i, j] == "E":
-#         FINAL_SCORES.append((score, visited))
-
-#     for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-#         if maze[i + di, j + dj] in [".", "E"] and (i + di, j + dj) not in visited:
-#             # visited.append((i + di, j + dj))
-#             if heading and (di, dj) != heading:
-#                 step_once(
-# i + di, j + dj, score=score+1001, visited=visited + [(i+di, j+dj)], heading=(di, dj))
-#             else:
-#                 step_once(
-# i + di, j + dj, score=score+1, visited=visited + [(i+di, j+dj)], heading=(di, dj))
+def get_valid_neighbors(heading):
+    return {
+        (0, 1): [(1, 0), (-1, 0), (0, 1)],
+        (0, -1): [(1, 0), (-1, 0), (0, -1)],
+        (1, 0): [(1, 0), (0, 1), (0, -1)],
+        (-1, 0): [(-1, 0), (0, 1), (0, -1)],
+    }[heading]
 
 
-#     return
-
-
-def dist(a, b):
-    (x1, y1) = a
-    (x2, y2) = b
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-
-
-FINAL_SCORES = []
-maze = np.array([list(x) for x in read_input(2024, 16, source="sample")])
-
-# G = nx.from_numpy_array(
-#     maze,
-#     create_using=nx.DiGraph,
-#     edge_attr="value"
-# )
-
+maze = np.array([list(x) for x in read_input(2024, 16, source="real")])
 
 start = np.argwhere(maze == "S")[0]
 end = np.argwhere(maze == "E")[0]
-nodes = deque([start[0], start[1], (0, 1), 0])
+heading = (0, 1)
+nodes = deque([(start[0], start[1], heading, 0, [])])
 best_score = 0
-# astar = nx.astar_path(G, source=tuple(start), target=tuple(end), heuristic=dist)
+visited = {}
 while nodes:
-    neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    for node in nodes:
-        pass
+    i, j, heading, score, history = nodes.pop()
+    visited[i, j, heading] = score
+
+    if score > best_score and best_score != 0:
+        continue
+
+    if maze[i, j] == "E" and (score <= best_score or best_score == 0):
+        if score < best_score or best_score == 0:
+            winning_paths = history
+            best_score = score
+            continue
+        elif score == best_score:
+            winning_paths.extend(history)
+            continue
+
+    for di, dj in get_valid_neighbors(heading):
+        if maze[i + di, j + dj] in [".", "E"] and (
+            (i + di, j + dj, heading) not in visited or visited[i + di, j + dj, heading] > score
+        ):
+            if (di, dj) == heading:
+                nodes.append((i + di, j + dj, heading, score + 1, history + [(i + di, j + dj)]))
+            else:
+                nodes.append((i + di, j + dj, (di, dj), score + 1001, history + [(i + di, j + dj)]))
 
 
-# step_once(*start, score=0, visited=[tuple(start)], heading=(0, 1))
-# print(f"PART 1: {min(x[0] for x in FINAL_SCORES)}")
+print(f"PART 1: {best_score}")
+print(f"PART 2: {len(set(winning_paths)) + 1}")
