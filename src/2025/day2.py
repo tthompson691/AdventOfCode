c80d30fb8d0346abf08c7753131b8db39acdada7
tdthompson <tdthompson@eprod.com>
2025-12-03 16:03:00 -0600

    pointless day 2 cleanup

--- src/2025/day2.py
+++ src/2025/day2.py
@@ -1,37 +1,29 @@
 from utils.utils import read_input
 
 
-def p2_check(val):
-    for stepsize in range(1, (len(val) // 2) + 1):
+def check_repeats(val, part):
+    if len(val) == 1:
+        return 0
+
+    range_start = {1: len(val) // 2, 2: 1}[part]
+    for stepsize in range(range_start, (len(val) // 2) + 1):
         splits = [val[a : a + stepsize] for a in range(0, len(val), stepsize)]
         if len(set(splits)) == 1:
-            return True
+            return int(val)
 
-    return False
-
-
-def p1_check(val):
-    middle_index = int(len(val) / 2)
-    return val[:middle_index] == val[int(middle_index) :]
+    return 0
 
 
-def day2(input_data):
+def day2(input_data, part):
     id_ranges = [
         range(int(x.split("-")[0]), int(x.split("-")[1]) + 1) for x in input_data.split(",")
     ]
-    p1_res = 0
-    p2_res = 0
-    for id_range in id_ranges:
-        for val in id_range:
-            if p1_check(str(val)):
-                p1_res += val
-            if p2_check(str(val)):
-                p2_res += val
+    res = sum([check_repeats(str(y), part) for x in id_ranges for y in x])
 
-    print(f"Part 1: {p1_res}")
-    print(f"Part 2: {p2_res}")
+    print(f"Part {part}: {res}")
 
 
 if __name__ == "__main__":
     input_data = read_input(2025, 2, source="real")[0]
-    day2(input_data)
+    for part in (1, 2):
+        day2(input_data, part)
