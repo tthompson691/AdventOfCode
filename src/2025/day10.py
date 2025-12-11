fe91d2f571f25d1566a18405aa5e1cbdc1c36294
tdthompson <tdthompson@eprod.com>
2025-12-11 16:21:53 -0600

    day 10 p1 done

--- src/2025/day10.py
+++ src/2025/day10.py
@@ -3,7 +3,7 @@ from collections import deque
 from utils.utils import read_input
 
 
-def press_one_button(button_pressed, current_state=None, num_presses=0):
+def p1_press_one_button(button_pressed, current_state=None, num_presses=0):
     flipper_dict = {".": "#", "#": "."}
 
     for i in button_pressed:
@@ -17,25 +17,68 @@ def part1(machines):
     for machine in machines:
         queue = deque([(["."] * len(machine["lights"]), 0)])  # (current_state, num_presses)
         state_achieved = False
-        # while current_state != machine["lights"]:
+        achieved_states = []
+
         while not state_achieved:
-            state, num_presses = queue.popleft()
+            state, past_presses = queue.popleft()
             for button in machine["buttons"]:
-                new_state, num_presses = press_one_button(
-                    button_pressed=button, current_state=state, num_presses=num_presses
+                new_state, num_presses = p1_press_one_button(
+                    button_pressed=button, current_state=state.copy(), num_presses=past_presses
                 )
+
+                if new_state in achieved_states:
+                    continue
+
+                achieved_states.append(new_state)
                 if new_state == machine["lights"]:
                     res += num_presses
                     state_achieved = True
                     break
-                queue.extend([(new_state, num_presses)])
+                queue.extend([(new_state.copy(), num_presses)])
 
     print(f"Part 1: {res}")
-    return
+
+
+def p2_press_one_button(button_pressed, current_state=None, num_presses=0):
+    for i in button_pressed:
+        current_state[i] += 1
+
+    return current_state, num_presses + 1
+
+
+def part2(machines):
+    # doesn't scale :(
+    res = 0
+    for i, machine in enumerate(machines):
+        queue = deque([([0] * len(machine["joltage"]), 0)])  # (current_state, num_presses)
+        state_achieved = False
+        achieved_states = []
+        desired_state = machine["joltage"]
+        while not state_achieved:
+            state, past_presses = queue.popleft()
+            for button in machine["buttons"]:
+                new_state, num_presses = p2_press_one_button(
+                    button_pressed=button, current_state=state.copy(), num_presses=past_presses
+                )
+
+                if new_state in achieved_states or any(
+                    nv > jv for nv, jv in zip(new_state, machine["joltage"])
+                ):
+                    continue
+
+                achieved_states.append(new_state)
+                if new_state == desired_state:
+                    res += num_presses
+                    state_achieved = True
+                    print(f"Machine {i} done")
+                    break
+                queue.extend([(new_state.copy(), num_presses)])
+
+    print(f"Part 2: {res}")
 
 
 if __name__ == "__main__":
-    data = read_input(2025, 10, source="sample")
+    data = read_input(2025, 10, source="real")
     machines = []
     for row in data:
         stuff = row.split()
@@ -46,8 +89,9 @@ if __name__ == "__main__":
                     (x,) if not isinstance(x, tuple) else x
                     for x in [eval(button) for button in stuff[1:-1]]
                 ],
-                "joltage": eval(stuff[-1]),
+                "joltage": eval(stuff[-1].replace("{", "[").replace("}", "]")),
             }
         )
 
     part1(machines)
+    part2(machines)
