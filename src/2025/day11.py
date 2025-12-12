cdc0070d47e762ef7dce6c6173558443a89c4fc8
tdthompson <tdthompson@eprod.com>
2025-12-12 12:10:35 -0600

    day 11 p2 progress

--- src/2025/day11.py
+++ src/2025/day11.py
@@ -12,34 +12,67 @@ def part1(devices):
         for destination in destinations:
             G.add_edge(source, destination)
 
-    p2 = None
+    print(f"Part 1: {len(list(nx.all_simple_paths(G, 'you', 'out')))}")
 
-    path_pairs = [
-        ["svr", "fft"],
-        ["svr", "dac"],
-        ["fft", "dac"],
-        ["dac", "fft"],
-        ["dac", "out"],
-        ["fft", "out"],
-    ]
-    for start, end in path_pairs:
-        paths = list(nx.all_simple_paths(G, start, end))
-        if paths == []:
-            continue
-        if not p2:
-            p2 = len(paths)
+
+def travel_to_next(
+    start,
+    end,
+    devices,
+    cache,
+):
+    destinations = devices[start]
+    for destination in destinations:
+        if destination == "out" and end != "out":
+            return 0
+        if destination == end:
+            return 1
+        elif destination in cache:
+            return cache[destination]
         else:
-            p2 *= len(paths)
+            cache[destination] = 0
+            cache[destination] += travel_to_next(destination, end, devices, cache)
 
-    # paths = list(nx.all_simple_paths(G, "svr", "out"))
-    # paths = [p for p in paths if "fft" in p and "dac" in p]
-    # p2 = len(paths)
+    return cache
 
-    print(f"Part 1: {p2}")
-    # print(f"Part 2: {len([p for p in all_paths if required_nodes.issubset(p)])}")
+
+def part2(devices):
+    """
+    1) nx graph does not scale for part 2
+    2) there are no paths from 'dac' to 'fft', even though the prompt says there might be
+    3) we have to find 3 separate path groups and multiply their counts together:
+       - from 'svr' to 'fft'
+       - from 'fft' to 'dac'
+       - from 'dac' to 'out'
+    4) going to try memoized recursion
+    """
+
+    node_pairs = [("svr", "fft"), ("fft", "dac"), ("dac", "out")]
+
+    for start, end in node_pairs:
+        res = travel_to_next(start, end, devices, {})
+        res
+
+
+#         # if cur_node == end:
+#         #     return 1
+
+# def part2b(devices):
+#     start = "svr"
+#     # end = "fft"
+#     stuff = []
+#     stack = ["fft"]
+#     while stack:
+#         end = stack.pop()
+#         valid_sources = [d for d in devices if end in devices[d]]
+#         stack.extend([d for d in valid_sources if d != start])
+#         stuff.append(len(valid_sources))
+
+#     stuff
 
 
 if __name__ == "__main__":
-    data = read_input(2025, 11, source="real")
-    devices = [(x, y.split(" ")) for x, y in (line.split(": ") for line in data)]
-    part1(devices)
+    data = read_input(2025, 11, source="sample")
+    devices = {x: y.split(" ") for x, y in (line.split(": ") for line in data)}
+    # part1(devices)
+    part2(devices)
